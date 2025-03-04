37a175df57e959908367f20bef62a90647bd764b
Didier_Defrance <didier@d-defrance.fr>
2025-03-04 10:46:50 +0100

    avec @angular/ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,10 +6,11 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { myAuthInterceptor } from './common/interceptor/my-auth.interceptor';
 import { provideOAuthClient } from 'angular-oauth2-oidc';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
     provideHttpClient(  withInterceptors([myAuthInterceptor]) )  ,
-    provideOAuthClient() /* instead of imports:[OAuthModule.forRoot()] */ 
+    provideOAuthClient(), provideClientHydration(withEventReplay()) /* instead of imports:[OAuthModule.forRoot()] */ 
   ]
 };
