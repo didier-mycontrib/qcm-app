13e69c67564247b9e47717afafc0e719993b1030
Didier_Defrance <didier@d-defrance.fr>
2025-08-29 11:47:07 +0200

    s

--- src/app/admin-qcm/qcm-editor/qcm-editor.component.ts
+++ src/app/admin-qcm/qcm-editor/qcm-editor.component.ts
@@ -20,6 +20,10 @@ export class QcmEditorComponent implements OnInit , OnChanges{
   constructor() { 
   }
 
+  //NB: ngOnChanges est appelé lorsque l'on change de qcm (sélection , chargement)
+  //le code si dessous balaye et analyse les parties.solutions
+  //pour recalculer (mettre à jour) la partie temporaire 
+  //this.qcm.questions[i].answers[j].ok (ce qui a pour effet de contrôler si une case est cohée ou pas=)
   ngOnChanges(changes: SimpleChanges): void {
     if(this.qcm==null) return;
     for(let i=0;i<this.qcm.nbQuestions;i++){
@@ -31,7 +35,7 @@ export class QcmEditorComponent implements OnInit , OnChanges{
             goodResp = true;
         }
         this.qcm.questions[i].answers[j].ok=goodResp;
-        console.log("i="+i+",j="+j+":"+this.qcm.questions[i].answers[j].ok);
+        //console.log("i="+i+",j="+j+":"+this.qcm.questions[i].answers[j].ok);
    }
  }
   }
@@ -39,8 +43,20 @@ export class QcmEditorComponent implements OnInit , OnChanges{
   ngOnInit() {
   }
 
+  //NB: invoked by (ngModelChange)="onUpdateSolutions(i,j) on checkbox (good answer or not)
   onUpdateSolutions(i:number,j:number){
-    
+     //console.log("onUpdateSolutions called with i="+i+",j="+j);
+      if(this.qcm==null) return;
+      let goodAnswerNums=[]
+      // on balaye (via indice j) toutes les checkbox associées à la question courante numero i
+      for(let j=0;j<this.qcm.questions[i].answers.length;j++){
+          if(this.qcm.questions[i].answers[j].ok){
+             goodAnswerNums.push(this.tabResNumFromIndex[j])
+          }
+      }
+     //console.log("goodAnswerNums="+goodAnswerNums)
+      this.qcm.solutions[i].goodAnswerNums=goodAnswerNums
+      //console.log("updated solution="+JSON.stringify(this.qcm.solutions[i]))
   }
 
   addNewQuestion(){
@@ -48,12 +64,18 @@ export class QcmEditorComponent implements OnInit , OnChanges{
     let currNbQuestions = this.qcm.questions.length;
     this.qcm.questions.push(new Question(++currNbQuestions));
     this.qcm.nbQuestions=this.qcm.questions.length;
+    if(this.qcm.solutions==null)
+       this.qcm.solutions=[]
+    this.qcm.solutions.push({num:this.qcm.nbQuestions , goodAnswerNums:[]})
+   // console.log("addNewQuestion called , this.qcm="+JSON.stringify(this.qcm))
   }
 
   removeLastQuestion(){
     if(this.qcm==null) return;
     this.qcm.questions.pop();
     this.qcm.nbQuestions=this.qcm.questions.length;
+    this.qcm.solutions.pop();
+    //console.log("removeLastQuestion called , this.qcm="+JSON.stringify(this.qcm))
   }
 
 }
